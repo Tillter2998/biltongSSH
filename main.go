cd35e225943326ba6efc9bbabb895e03fada37f6
Tillter2998 <elrich.dev2998@gmail.com>
2026-09-11 10:58:26 -0300

    Setup local server and surface biltongTUI to connected users

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	tui "github.com/Tillter2998/biltongTUI"
+
+	tea "charm.land/bubbletea/v2"
+	"charm.land/log/v2"
+	"charm.land/ssh"
+	"charm.land/wish/v2"
+	"charm.land/wish/v2/activeterm"
+	"charm.land/wish/v2/bubbletea"
+	"charm.land/wish/v2/logging"
+)
+
+const (
+	host = "localhost"
+	port = "23234"
+)
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			bubbletea.Middleware(teaHandler),
+			activeterm.Middleware(),
+			logging.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH Server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH Server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
+	}
+}
+
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	return tui.NewModel(), []tea.ProgramOption{}
+}
